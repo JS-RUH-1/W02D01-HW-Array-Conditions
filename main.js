da0109264a8e4952513d0c9c8d67623a5adc8b22
Shahad-F <shahadbinsweleh@gmail.com>
2021-10-15 22:15:20 +0300

    After Update

--- main.js
+++ main.js
@@ -29,248 +29,123 @@ const characters = [
     },
 ];
 
-// For loop
-
-console.log("For loop")
-console.log("Numbers' summations ////")
-
-let sumHeight =0;
-let sumMass =0;
-
-for (let i=0 ;i< characters.length;i++){
-     sumHeight=sumHeight+characters[i].height;
-   
-     sumMass= sumMass+characters[i].mass;
-}
-console.log(" Sum of all height is :("+sumHeight+")")
-console.log(" Sum of all mass is :("+sumMass+")")
-
-console.log("----------------------")
-
-
-let Number =[23,54,32,87,47];
-
-let sum=0;
-for(let i=0 ; i<Number.length;i++){
-
-    sum =sum+Number[i];
-}
-
-console.log(" Sum of all Number is :"+sum)
-
-console.log("----------------------")
-
-console.log("Maximum number///")
-
-let num=[16 ,4,2,0,19,6];
-
-let max =num[0];
-
-for(let i= 1 ;i<num.length;i++){
-if (max <num[i])
-max=num[i]
-
-}
-console.log("The max number is :"+max)
-
-console.log("----------------------")
-
-console.log("Reverse Array////")
-
-console.log( "Reverse Array is :")
-
-let arr=[1,2,3,4,5,6,7,8,9,10];
-
-for (let i=arr.length-1 ; i>=0; i--){
-     console.log(arr[i])
-}
-
-console.log("----------------------")
-
-console.log("Nested for loop//")
-
-console.log("stars pattern //")
-
-let star =5;
-let string =""
-for( let i=1; i <=star ;i++){
- for (let j=1 ; j<=i ;j++){
-     string+="*"
- }
- string += "\n";
-}
-console.log(string);
-console.log("----------------------")
-
-console.log("While loop ///")
-
-let wh=7 ,flag =true;
-let i=2;
-while(i<= wh-1){
-
-    if (wh % i ==0)
-           flag=false;
-            break
-}
-if (flag == true)
-console.log(wh + " is prime");
-else
-console.log(wh+ " is not prime");
-console.log("----------------------")
-
-console.log("Arrays////")
-
-console.log("MAP ////")
-
-
+ 
 // MAP
 console.log("Map ..")
-for (let i=0 ;i<characters.length;i++){
-console.log("Name is :["+characters[i].name +"] the Height is : ["+ characters[i].height+"]")
-console.log("----------------")
+
+ console.log('Get array of all names')
+console.log( characters.map(Map));
+
+ function Map(item){
+     return[item.name].join(' ');
+ }
+ console.log("----------------")
+
+ 
+ 
+console.log('Get array of all heights')
+
+console.log(characters.map(Map2))
+ 
+
+function Map2(item){
+    return[item.height].join(' ');
 }
+console.log("----------------")
 
 //REDUCE
-console.log("REDUCE ..")
-let SUM =0;
+console.log('REDUCE')
+console.log('Get total height of all characters')
+console.log("----------------")
+ 
+console.log(characters.reduce(Reduce,0));
+ 
 
-for(let i=0 ;i<characters.length ;i++){
+function Reduce(total,num){
+     
 
-    SUM= SUM + characters[i].mass
-
-
+return  total + num.height; 
 }
-console.log("Total Mass is :"+SUM)
 console.log("----------------")
 
 
-//  
-// let count =0
-
-//  for( let i =0 ;i<characters.length ;i++)
-//     {
-// if (characters[i].eye_color =="blue")
-//     count = count+1;
-
-// }
-
-// console.log("The total of blue eye is :"+ count)
-// console.log("----------------")
-
+ 
 //FILTER
 console.log("FILTER .... ")
-for (let i=0 ;i< characters.length ;i++){
+console.log("----------------")
+ console.log('Get characters with height less than 200')
+
+console.log(characters.filter(Filter));
+
+function Filter(check){
+    return check.height <200;
+}
 
 
-    if (characters[i].mass > 100)
 
-    console.log(" Heigth less than 100  is :["+characters[i].mass+"]")
+console.log("----------------")
+
+console.log('Get all male characters')
+
+console.log(characters.filter(Filter2));
+
+function Filter2(check){
+    return check.gender =='male';
+}
+
+console.log("----------------")
+
+console.log('SORT')
+console.log('Sort by mass')
+
+
+console.log(characters.sort(SORT))
+function SORT(a,b){
+    return a.mass -b.mass;
+}
+console.log("----------------")
+console.log('Sort by height')
+
+console.log(characters.sort(SORT2))
+
+function SORT2(a,b){
+    return a.height -b.height;
 }
 console.log("----------------")
 
-for (let i=0 ;i<characters.length ;i++){
+console.log('EVERY')
+console.log('Does every character have mass more than 40?')
 
-if (characters[i].gender =="female")
-console.log("["+characters[i].name +"] She Is a female ")
- 
+console.log(characters.every(EV))
+function EV(check){
+
+    return check.mass >40;
+}
+console.log("----------------")
+console.log('Is every character shorter than 200?')
+
+console.log(characters.every(EV))
+function EV(check){
+
+    return check.height <200;
 }
 console.log("----------------")
 
-//SORT
-
-console.log("SORT the name ")
-
  
-    
-   function SortName(a,b){
+console.log('SOME')
+console.log('Is there at least one character with blue eyes?')
 
-    if (a.name < b.name){return -1;}
-    if (a.name < b.name){return 1;}
-    return 0;
-   }
+console.log(characters.some(SO))
 
-   let so= characters.sort(SortName);
-   console.log(so)
+function SO (ch){
+    return ch.eye_color=="blue";
+}
 
-   console.log("----------------")
-   console.log("SORT the Gender ")
+console.log("----------------")
 
+console.log('Is there at least one character taller than 210?')
+console.log(characters.some(SO2))
 
-   function SortGender(a,b){
-
-    if (a.gender < b.gender){return -1;}
-    if (a.gender < b.gender){return 1;}
-    return 0;
-   }
-
-   let soGen= characters.sort(SortGender);
-   console.log(soGen)
-
-     console.log("----------------")
-
-
-//      // EVERY
-
-     console.log("EVERY")
-     console.log("Does every character have blue eyes?")
-
-     let blueColor= false;
-
-     for( let i =0 ; i< characters.length ;i++){
- 
-         if(characters[i].eye_color== "blue"){
-                 blueColor= true;
-         
-        }
-        break
-     }
-       console.log(blueColor)
-       console.log("----------------")
- 
-       console.log("Is every character male?")
- 
-
-       let male= false;
-
-     for( let i =0 ; i< characters.length ;i++){
- 
-         if(characters[i].gender =="male"){
-                 male= true;
-         
-        }
-        break
-     }
-       console.log(male)
-       console.log("----------------")
-
-
-// //SOME
-
-console.log("SOME")
-
-console.log("Is there at least one male character?")
-    let oneMale= false;
-
-    for( let i =0 ; i< characters.length ;i++){
-
-        if(characters[i].gender =="male"){
-            oneMale= true;
-        break}
-    }
-      console.log(oneMale)
-      console.log("----------------")
-
-
-      console.log("Is there at least one character that has mass less than 50?")
-      let mas= false;
-
-    for( let i =0 ; i< characters.length ;i++){
-
-        if(characters[i].mass <50){
-        mas= true;
-        break}
-    }
-      console.log(mas)
-      console.log("----------------")
-
-
+function SO2 (ch){
+    return ch.height > 210;
+}
